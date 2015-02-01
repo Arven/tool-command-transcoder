62aca592bf63fc636d5e04279ee8ef837dfe1093
Arven <brian.johnathan.b@gmail.com>
2015-01-31 19:32:01 -0500

    Very basic widget example for Ttk Treeview

---
+++ transcoder.tcl
@@ -0,0 +1,16 @@
+package require tile
+ttk::setTheme alt
+# create it:
+ttk::treeview .tree
+pack .tree -expand 1 -fill both
+# insert an entry at the root level:
+set entry1 [.tree insert {} end -text "first item"]
+# and a second one:
+set entry2 [.tree insert {} end -text "second item"]
+# insert a new level with entry1 as parent:
+.tree insert $entry1 end -text "a sublevel"
+# insert another item under this one:
+.tree insert $entry1 end -text "another item"
+# suppose, one item is selected.
+# we can then delete it thus:
+.tree delete [.tree selection]
